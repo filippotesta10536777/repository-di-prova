6e5f971af21f0ededb471525b405cc95a203d023
filippotesta10536777 <filippotesta8@gmail.com>
2018-06-13 19:18:14 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+jQuery('#mouse').hover(function() {
+
+jQuery('.background').addClass('show');
+})
+
+.mouseleave(function() {
+jQuery('.background').removeClass('show');
+});
+
+//foto 2
+jQuery('#mouse2').hover(function() {
+
+jQuery('.background2').addClass('show');
+})
+
+.mouseleave(function() {
+jQuery('.background2').removeClass('show');
+});
+
+//foto 3
+jQuery('#mouse3').hover(function() {
+
+jQuery('.background3').addClass('show');
+})
+
+.mouseleave(function() {
+jQuery('.background3').removeClass('show');
+});
+
+//foto 4
+jQuery('#mouse4').hover(function() {
+
+jQuery('.background4').addClass('show');
+})
+
+.mouseleave(function() {
+jQuery('.background4').removeClass('show');
+});
